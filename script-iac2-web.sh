6e662f350c105624fe1313cac07b8afb0ad12517
jxvz <nascimento.joaovitor@gmail.com>
2022-08-25 05:49:59 +0000

    Apache v.1.0

---
+++ script-iac2-web.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "Atualizando o servidor..."
+apt-get uptate
+apt-get upgrade -y
+
+echo "Instalando Apache e unzip..."
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando arquivos da aplicação..."
+
+cd/tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
